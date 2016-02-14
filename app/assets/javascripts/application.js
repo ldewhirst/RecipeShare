3f174941981620a5127a8f3ed8928583ba03ebbd
Lindsay Dewhirst <lindsay.dewhirst@gmail.com>
2016-02-14 08:06:08 -0500

    Javascript Edit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,13 @@
+//= require jquery
+//= require jquery.turbolinks
+//= require jquery_ujs
+//= require bootsy
+//= require masonry/jquery.masonry
+//= require masonry/jquery.event-drag
+//= require masonry/jquery.imagesloaded.min
+//= require masonry/jquery.infinitescroll.min
+//= require masonry/modernizr-transitions
+//= require masonry/box-maker
+//= require turbolinks
+//= require bootstrap
+//= require_tree .
